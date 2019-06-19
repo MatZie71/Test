138329ccc86766e1ec40b86529b2d0a3d38efdb5
Mateusz <mat3uszx@gmail.com>
2019-06-19 19:51:44 +0200

    [GIT] GitObject added

---
+++ src/GitObiekt.java
@@ -0,0 +1,16 @@
+public class GitObiekt {
+    private String nazwa = "?";
+
+    public GitObiekt() {
+        System.out.println("I'm created");
+    }
+
+    public GitObiekt(String nazwa) {
+        System.out.println("I'm "+nazwa +"!!!");
+        this.nazwa = nazwa;
+    }
+
+    public void przedstawSie() {
+        System.out.println("Jestem " + nazwa);
+    }
+}
--- src/GitTest.java
+++ src/GitTest.java
@@ -1,2 +1,11 @@
 public class GitTest {
+
+    public static void main(String[] args) {
+        GitObiekt obiekt = new GitObiekt();
+        GitObiekt obiekt2 = new GitObiekt("Mateusz");
+        obiekt.przedstawSie();
+        obiekt2.przedstawSie();
+    }
 }
+
+
